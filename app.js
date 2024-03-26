c4be1d84a219a86be0462a7b08a2507c36901225
MabyDiaz <mabeldiaz7680@gmail.com>
2024-03-26 20:35:08 -0300

    modifico app.js

--- app.js
+++ app.js
@@ -6,14 +6,16 @@ Nota es menor a 7 y no es LIBRE - REGULAR
 Nota mayor que 6  - PROMOCIONADO. */
 
 function calcularEstado(nota) {
-  if (nota >= 4 && nota < 7) {
+  if (nota < 4) {
+    return "LIBRE";
+  } else if (nota < 7 && nota >= 4) {
     return "REGULAR";
-  } else if (nota > 6) {
+  } else if (nota > 6 && nota <= 10) {
     return "PROMOCIONADO";
   } else {
-    return "LIBRE";
+    return "Nota no válida. INgrese un número válido para la nota";
   }
 }
 
-let estado = calcularEstado(7);
+let estado = calcularEstado(3);
 console.log(estado);
